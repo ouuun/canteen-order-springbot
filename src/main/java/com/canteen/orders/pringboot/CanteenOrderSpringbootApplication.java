6ab149b570b0ec84b6e9ae99c3a315fc4f464ff1
ouuun <954574569@qq.com>
2023-05-28 22:46:30 +0800

    first commit

---
+++ src/main/java/com/canteen/orders/pringboot/CanteenOrderSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.canteen.orders.pringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanteenOrderSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanteenOrderSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/canteen/orders/pringboot/CanteenOrderSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canteen.orders.pringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanteenOrderSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
